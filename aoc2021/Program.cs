b4ed8aba39d98c2e8ef4a00f418cb7b5a93a80b7
Wouter <wouter075@gmail.com>
2021-11-29 10:53:23 +0100

    Initial commit

---
+++ aoc2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aoc2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We are going to try to do this in C#!");
+        }
+    }
+}
