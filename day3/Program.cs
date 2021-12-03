ab3a70d1c80d13f32f92cb9a1c9f70d7eb2261ca
Wouter <wouter075@gmail.com>
2021-12-03 11:49:25 +0100

    Day 3: Done!

---
+++ day3/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = System.IO.File.ReadAllLines(@"../../../day3.txt");
+            var gamma = "";
+            var epsilon = "";
+            var gammaCount = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            foreach (var line in lines)
+            {
+                for (var i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == '1')
+                    {
+                        gammaCount[i] += 1;
+                    }
+                }
+            }
+
+            foreach (var gc in gammaCount)
+            {
+                if (gc > 500)
+                {
+                    gamma += "1";
+                    epsilon += "0";
+                }
+                else
+                {
+                    gamma += "0";
+                    epsilon += "1";
+                }
+            }
+
+            var gammaDec = Convert.ToInt64(gamma, 2);
+            var epsilonDec = Convert.ToInt64(epsilon, 2);
+            var calc = gammaDec * epsilonDec;
+            Console.WriteLine($"Part 1 {calc}");
+
+            List<string> newList = lines.OfType<string>().ToList();
+
+            while (newList.Count != 1)
+            {
+                for (int x = 0; x < lines[0].Length; x++)
+                {
+                    newList = PopOxygen(newList, x);
+                }
+            }
+            var oxygenDec = Convert.ToInt64(newList[0], 2);
+            
+            newList = lines.OfType<string>().ToList();
+            while (newList.Count != 1)
+            {
+                for (int x = 0; x < lines[0].Length; x++)
+                {
+                    newList = PopCo2(newList, x);
+                    if (newList.Count == 1)
+                    {
+                        break;
+                    }
+                }
+            }
+            var o2Dec = Convert.ToInt64(newList[0], 2);
+            
+            Console.WriteLine($"Part 2: {oxygenDec * o2Dec}");
+        }
+
+        static List<String> PopOxygen(List<string> lines, int bit)
+        {
+            // count the numbers of the bit:
+            var mostCommon = '0';
+            var oneCount = lines.Count(line => line[bit] == '1');
+            var zeroCount = lines.Count - oneCount;
+
+            // determine the most common bit
+            if (oneCount > zeroCount)
+            {
+                mostCommon = '1';
+            }
+
+            // if they are the same, 1 wins
+            if (oneCount == zeroCount)
+            {
+                mostCommon = '1';
+            }
+
+            // filter the list and push back
+            return lines.Where(line => line[bit] == mostCommon).ToList();
+        }
+
+        static List<string> PopCo2(List<string> lines, int bit)
+        {
+            var leastCommon = '0';
+            var oneCount = lines.Count(line => line[bit] == '1');
+            var zeroCount = lines.Count - oneCount;
+            if (oneCount < zeroCount)
+            {
+                leastCommon = '1';
+            }
+
+            if (oneCount == zeroCount)
+            {
+                leastCommon = '0';
+            }
+
+            return lines.Where(line => line[bit] == leastCommon).ToList();
+        }
+    }
+}
