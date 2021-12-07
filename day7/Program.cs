a65ac95fa1ff6f17e8d2789681a0c0c5930271cc
Wouter <wouter075@gmail.com>
2021-12-07 11:04:46 +0100

    Day 7 - Part 1 & 2 - Bruteforce

---
+++ day7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = System.IO.File.ReadAllLines(@"day7.txt");
+            var values = lines[0].Split(",").Select(x => Convert.ToInt32(x)).ToList();
+
+            var fuel1 = Int32.MaxValue;
+            var fuel2 = Int32.MaxValue;
+            for (var y = 1; y <= values.Count; y++)
+            {
+                var tmp1 = 0;
+                var tmp2 = 0;
+                foreach (var steps in values.Select(t => Math.Abs(t - y)))
+                {
+                    tmp1 += steps;
+                    for (var i = 0; i <= steps; i++) tmp2 += i;
+                }
+                if (tmp1 < fuel1) fuel1 = tmp1;
+                if (tmp2 < fuel2) fuel2 = tmp2;
+            }
+            Console.WriteLine($"Part 1: {fuel1}");
+            Console.WriteLine($"Part 2: {fuel2}");
+        }
+    }
+}
